7e6bf29bf9934f23a008a4a301694360fa7b7078
Joe Chin <joe.chin@acquia.com>
2019-10-31 14:15:31 +0800

    Change task list command to list backup specific tasks only

--- src/Command/ListBackupTasks.php
+++ src/Command/ListBackupTasks.php
@@ -10,18 +10,25 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use SiteFactoryAPI\Config\ConfigFile;
 
-class ListTasks extends Command {
+class ListBackupTasks extends Command {
   /**
    * @inheritdoc
    */
   protected function configure() {
     $this
-      ->setName('task:list')
-      ->setDescription('List tasks.')
+      ->setName('backups:tasks')
+      ->setDescription('List backup tasks.')
       ->addArgument(
         'sitegroup',
         InputArgument::REQUIRED,
         'Combination of sitename and environment in one word. E.g. mystack01live.'
+      )
+      ->addOption(
+        'status',
+        's',
+        InputOption::VALUE_OPTIONAL,
+        'Filter by status. Allowed values: processing, error, not-started',
+        'all'
       )
       ->addOption(
         'limit',
@@ -29,7 +36,7 @@ class ListTasks extends Command {
         InputOption::VALUE_OPTIONAL,
         'A positive integer (max 100).',
         10
-      )
+      )      
       ->addOption(
         'page',
         'p',
@@ -50,7 +57,9 @@ class ListTasks extends Command {
     $response = $client->request('GET', "tasks", [
       'query' => [
         'page' => $input->getOption('page'),
-        'limit' => $input->getOption('limit')
+        'limit' => $input->getOption('limit'),
+        'status' => $input->getOption('status'),        
+        'group' => "SiteArchive",
       ]
     ]);
     $data = $response->getBody();
@@ -60,20 +69,29 @@ class ListTasks extends Command {
       return;
     }
 
-    // Hide some of the fields.
-    foreach ($data as $key => &$task) {
-      unset($data[$key]['lease']);
-      unset($data[$key]['object_id']);
-      unset($data[$key]['lease']);
-      unset($data[$key]['max_run_time']);
-      unset($data[$key]['concurrency_exceeded']);
-      unset($data[$key]['nid']);
-      unset($data[$key]['uid']);
-      unset($data[$key]['class']);
+    // Build formatted output.
+    $rows = [];
+    $status = [
+      4 => 'in progress',
+      8 => 'waiting',
+      16 => 'completed',
+      32 => 'error'
+    ];
+    
+    foreach ($data as $row) {
+      $rows[] = [
+        'id' => $row['id'],
+        'name' => $row['name'],
+        'priority' => $row['priority'],
+        'status' => $status[$row['status']],
+        'added' => date('Y-m-d h:i:s', $row['added']),
+        'started' => date('h:i:s', $row['started']),
+        'completed' => date('h:i:s', $row['completed']),
+        'paused' => $row['paused'],
+        'error_message' => $row['error_message'],
+      ];  
     }
-
-    $io->table(array_keys($data[0]), $data);
+    $io->table(array_keys($rows[0]), $rows);
   }
 }
 
- ?>
