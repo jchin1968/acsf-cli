2e60b69ba74a66b11a037dc77a306181ea4b1b67
Joe Chin <joe.chin@acquia.com>
2020-02-06 13:18:17 +0800

    Combine single and bulk site backup feature into one command.

--- src/Command/CreateBackup.php
+++ src/Command/CreateBackup.php
@@ -17,16 +17,16 @@ class CreateBackup extends Command {
   protected function configure() {
     $this
       ->setName('backups:create')
-      ->setDescription('Create a site backup')
+      ->setDescription('Create site(s) backup')
       ->addArgument(
         'sitegroup',
         InputArgument::REQUIRED,
         'Combination of sitename and environment in one word. E.g. mystack01live.'
       )
       ->addArgument(
-        'site_id',
-        InputArgument::REQUIRED,
-        'Site ID'
+        'site_ids',
+        InputArgument::IS_ARRAY | InputArgument::REQUIRED,
+        'A list of Site IDs (space delimited) to backup. Use "all" to backup all primary sites.'
       )
       ->addOption(
         'label',
@@ -57,6 +57,13 @@ class CreateBackup extends Command {
         'c',
         InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY,
         'Components to backup. Possible values are: codebase, database, public files, private files and themes. Omitting this option will backup every component. To specify multiple components, prefix each one with -c.'
+      )
+      ->addOption(
+        'include_non_primary',
+        'p',
+        InputOption::VALUE_OPTIONAL,
+        'Include non-primary sites in the backup. Omitting this flag will backup primary sites only',
+        false
       );
   }
 
@@ -65,7 +72,7 @@ class CreateBackup extends Command {
    */
   protected function execute(InputInterface $input, OutputInterface $output) {
     $io = new SymfonyStyle($input, $output);
-    $site_id = $input->getArgument('site_id');
+    $site_ids = $input->getArgument('site_ids');
 
     $data = array_filter([
       'label' => $input->getOption('label'),
@@ -79,14 +86,46 @@ class CreateBackup extends Command {
 
     $client = ConfigFile::load($input->getArgument('sitegroup'))->getApiClient();
 
-    $response = $client->request('POST', "sites/$site_id/backup", [
-      'headers' => [
-        'Content-Type' => 'application/json'
-      ]
-    ]);
-    $data = $response->getBody();
-    $data = json_decode($data, TRUE);
+    // If first site_id argument is 'all' then fetch all sites to backup
+    if ($site_ids[0] == 'all') {
+      // Get a list of all sites.
+      $response = $client->request('GET', "sites", [
+        'query' => [
+          'page' => 1,
+          'limit' => 5000
+        ]
+      ]);
+      $response_body = $response->getBody();
+      $response_body = json_decode($response_body, TRUE);
+      $sites = $response_body['sites'];
 
-    $io->success("Task created: {$data['task_id']}");
+      // Rebuild $site_ids array.
+      $site_ids = [];
+      foreach ($sites as $site) {
+        // Only include primary sites unless include_non_primary flag is set.
+        $include_non_primary = $input->getOption('include_non_primary') !== false;
+        if ($site['is_primary'] || $include_non_primary) {
+            $site_ids[] = $site['id'];
+        }
+      }
+
+      // Exit if no sites are found.
+      if (empty($site_ids)) {
+        $io->warning('No sites found');
+        return;
+      }
+    }
+
+    // Create backup for each site.
+    foreach ($site_ids as $index => $site_id) {
+      $response = $client->request('POST', "sites/$site_id/backup", [
+        'headers' => [
+          'Content-Type' => 'application/json'
+        ],
+      ]);
+      $results = $response->getBody();
+      $results = json_decode($results, TRUE);
+      $io->success("Task created: {$results['task_id']}");
+    }
   }
 }
